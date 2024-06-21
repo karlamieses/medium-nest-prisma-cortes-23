43553a70b26e22f248090764a8b44cbf82d4cc45
karlamieses <karlamieses.km@gmail.com>
2024-06-21 14:31:01 +0100

    test(auth): add integration tests for login and signup functions

---
+++ cypress/support/commands.ts
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+import cypress = require("cypress");
+
+declare global {
+    namespace Cypress{ 
+        interface Chainable<Subject> {
+        getRequest(endpoint: string, token?: string): Chainable<Subject>;
+        postRequest(endpoint: string, body: unknown, token?: string): Chainable<Subject>;
+        putRequest(endpoint: string, body: unknown, token?: string): Chainable<Subject>;
+        deleteRequest(endpoint: string, token?: string): Chainable<Subject>;
+        }
+    }
+  }
+
+Cypress.Commands.add("getRequest", (endpoint: string, token?: string) => {
+  return cy.request({
+      method: "GET",
+      url: `${Cypress.env("baseUrl")}${endpoint}`,
+      headers: {
+          "Content-Type": "application/json", // Add Content-Type header
+          "Authorization": `Token ${token}`  // Conditionally add Authorization header
+      }
+  });
+});
+
+
+Cypress.Commands.add("postRequest", (endpoint, body: any, token) => {
+    return cy.request({
+      method: "POST",
+      url: `${Cypress.env('baseUrl')}${endpoint}`,
+      body,
+      failOnStatusCode: false,
+      ...(token && { headers: { Authorization: `Token ${token}` } }),
+    });
+  });
---
+++ cypress/support/generate.users.ts
@@ -0,0 +1,10 @@
+export function generateUniqueUser() {
+    const timestamp = Date.now();
+    const prefix = Cypress.env('prefix');
+    
+    return {
+        username: `${prefix}${timestamp}`,
+        email: `${prefix}${timestamp}@example.com`,
+        password: `${prefix}${timestamp}`,
+    };
+}
